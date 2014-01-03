173ccb1a23ed196b2d2648440d5ad55a83d703a1
Guillermo Infante Hernández <guillermo2104@gmail.com>
2014-01-03 18:27:22 +0100

    Initial commit. Adding setup scripts

---
+++ scripts/ckan.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+
+# --- Change this  default values ---: #
+postgres_user_pass=postgres
+ckan_user_name=ckan_default
+ckan_user_pass=default_pass
+ckan_db_name=ckan_default
+# ------------------------------------ #
+
+# Run-once script
+if [ -f "/vagrant/prov/ckan_provision" ]; then
+exit 0
+fi
+echo 'Drupal already configured!. Delete this to reconfigure Drupal stack.' > /vagrant/prov/ckan_provision
+
+# Install required packages:
+apt-get update
+apt-get install -y python-dev postgresql libpq-dev python-pip python-virtualenv git-core solr-jetty openjdk-6-jdk
+
+# Install CKAN into a Python virtual environment:
+mkdir -p /vagrant/ckan/lib
+sudo ln -s /vagrant/ckan/lib /usr/lib/ckan
+mkdir -p /vagrant/ckan/etc
+sudo ln -s /vagrant/ckan/etc /etc/ckan
+sudo mkdir -p /usr/lib/ckan/default
+sudo chown `whoami` /usr/lib/ckan/default
+virtualenv --no-site-packages /usr/lib/ckan/default
+. /usr/lib/ckan/default/bin/activate
+pip install -e 'git+https://github.com/okfn/ckan.git@ckan-2.0#egg=ckan'
+pip install -r /usr/lib/ckan/default/src/ckan/pip-requirements.txt
+deactivate
+. /usr/lib/ckan/default/bin/activate
+
+# Setup PostgreSQL database:
+sudo -u postgres psql -c"ALTER user postgres WITH PASSWORD '$postgres_user_pass'"
+sudo -u postgres dropdb postgres
+sudo -u postgres createdb -O postgres postgres --lc-ctype en_US.utf8  --lc-collate en_US.utf8 -E utf-8 -T template0
+
+# Create ckan user:
+sudo -u postgres createuser -S -D -R $ckan_user_name
+sudo -u postgres psql -c"ALTER user $ckan_user_name WITH PASSWORD '$ckan_user_pass'"
+
+# Creates ckan db:
+sudo -u postgres createdb -O $ckan_user_name $ckan_db_name --lc-ctype en_US.utf8  --lc-collate en_US.utf8 -E utf-8 -T template0
+
+# Create a CKAN config file:
+sudo mkdir -p /etc/ckan/default
+sudo chown -R `whoami` /etc/ckan/
+cd /usr/lib/ckan/default/src/ckan
+paster make-config ckan /etc/ckan/default/development.ini
+
+# Edit the development.ini file changing the following options:'
+sed -i 's/ckan_default:pass/'$ckan_user_name':'$ckan_user_pass'/g' /etc/ckan/default/development.ini
+
+# Setup Solr (Single Solr instance):
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/jetty
+sed -i 's|#JETTY_HOST=$(uname -n)|JETTY_HOST=127.0.0.1|g' /etc/default/jetty
+sed -i 's|#JETTY_PORT=8080|JETTY_PORT=8983|g' /etc/default/jetty
+sed -i 's|#JAVA_HOME=|JAVA_HOME=/usr/lib/jvm/java-6-openjdk-i386/|g' /etc/default/jetty
+sudo service jetty start
+sudo mv /etc/solr/conf/schema.xml /etc/solr/conf/schema.xml.bak
+sudo ln -s /usr/lib/ckan/default/src/ckan/ckan/config/solr/schema-2.0.xml /etc/solr/conf/schema.xml
+sudo service jetty restart
+
+# Create database tables:
+cd /usr/lib/ckan/default/src/ckan
+paster db init -c /etc/ckan/default/development.ini
+
+# 7. Set up the DataStore
+# TODO
+
+# Link to who.ini:
+ln -s /usr/lib/ckan/default/src/ckan/who.ini /etc/ckan/default/who.ini
+
+# Deploy CKAN using Apache and modwsgi
+# Create a production.ini File
+cp /etc/ckan/default/development.ini /etc/ckan/default/production.ini
+
+# Install Apache and modwsgi
+apt-get install -y apache2 libapache2-mod-wsgi
+
+# Move Apache to /vagrant directory
+rm -rf /var/www
+ln -fs /vagrant /var/www
+
+# Create the WSGI Script File
+cp /vagrant/scripts/apache.wsgi /etc/ckan/default/apache.wsgi
+
+# Stablish read permisions
+sudo chmod -R 755 /var/www
+
+# Create the Apache Config File
+cp /vagrant/scripts/vhost /etc/apache2/sites-available/default_vhost
+
+# Enable the ckan_site
+sudo a2ensite default_vhost
+sudo a2dissite default
+
+# Reload apache
+sudo service apache2 reload
+
+
+
+
+
+
+
+
+
---
+++ scripts/drupal.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+# --- Change this  default values ---: #
+mysql_root_pass=root
+app_pass=root
+drupal_db=drupal
+drupal_user_name=druser
+drupal_user_pass=root
+drupal_folder_name=landportal
+drupal_site_name=The-Land-Portal
+drupal_account_name=admin
+drupal_account_pass=admin
+# ------------------------------------ #
+
+# Run-once script
+if [ -f "/vagrant/prov/drupal_provision" ]; then
+exit 0
+fi
+echo 'Drupal already configured!. Delete this to reconfigure Drupal stack.' > /vagrant/prov/drupal_provision
+
+# Update system
+apt-get update
+
+# Install PHP
+sudo apt-get install -y php5 libapache2-mod-php5 php5-cli php5-mysql
+sudo service apache2 restart
+
+# Install MySQL
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password '$mysql_root_pass''
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password '$mysql_root_pass''
+apt-get install -y mysql-server mysql-client libmysqlclient-dev
+
+# Install phpMyAdmin
+sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/dbconfig-install boolean true'
+sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/app-password-confirm password '$app_pass''
+sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/admin-pass password '$mysql_root_pass''
+sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/app-pass password '$mysql_root_pass''
+sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2'
+apt-get install -y phpmyadmin
+sudo ln -s /usr/share/phpmyadmin /var/www/
+
+# Install Drush
+apt-get install -y php-pear
+sudo pear channel-discover pear.drush.org
+sudo pear install drush/drush
+wget http://download.pear.php.net/package/Console_Table-1.1.3.tgz
+sudo tar zxvf Console_Table-1.1.3.tgz -C /usr/share/php/drush/lib
+sudo pear upgrade drush/drush
+
+# Install server requirements for Drupal
+apt-get install -y php5-gd
+a2enmod rewrite
+sudo service apache2 restart
+
+# Deploying a new Drupal site with Drush
+# Create Drupal db
+mysql -h localhost -u root -p$mysql_root_pass -e "create database $drupal_db"
+
+# Create drupal db user
+mysql -h localhost -u root -p$mysql_root_pass $drupal_db -e "create user $drupal_user_name@localhost"
+
+# Set password to drupal db user
+mysql -h localhost -u root -p$mysql_root_pass $drupal_db -e "set password for $drupal_user_name@localhost= password ('$drupal_user_pass')"
+
+# Grant all privileges to drupal db user
+mysql -h localhost -u root -p$mysql_root_pass $drupal_db -e "grant all privileges on $drupal_db.* to $drupal_user_name@localhost identified by '$drupal_user_pass' with grant option"
+
+# Refresh MySQL
+mysql -h localhost -u root -p$mysql_root_pass $drupal_db -e "flush privileges"
+
+# Navigate to www folder
+cd /var/www
+
+# Download Drupal
+drush dl drupal --drupal-project-rename=$drupal_folder_name
+
+# Install Drupal
+cd /var/www/$drupal_folder_name
+drush -y site-install standard --db-url=mysql://$drupal_user_name:$drupal_user_pass@localhost/$drupal_db --site-name=$drupal_site_name --account-name=$drupal_account_name --account-pass=$drupal_account_pass
+
+# Assign (sites/default/files) group ownership to the www-data group
+sudo chown -R root:www-data sites/default/files
+
+# Enable www-data group to write in sites/default/files
+sudo chmod -R 775 sites/default/files
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
