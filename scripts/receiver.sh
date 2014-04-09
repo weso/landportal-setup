e04fea6485b266592f230bb2226aa9d588ea4d3f
belaustegui <cristian.alvb@gmail.com>
2014-04-09 23:29:47 +0200

    Update installation scripts. See #20

--- scripts/receiver.sh
+++ scripts/receiver.sh
@@ -6,19 +6,20 @@ exit 0
 fi
 echo 'Receiver already configured!. Delete this to reconfigure Receiver stack.' > /vagrant/receiver_configured
 
-# Install Receiver into a Python virtual environment:
-mkdir -p /vagrant/virtualenvs/receiver
-virtualenv /vagrant/virtualenvs/receiver
-. /vagrant/virtualenvs/receiver/bin/activate
-cd /vagrant/virtualenvs/receiver
-mkdir src
-cd src
-git clone https://github.com/weso/landportal-receiver.git receiver
-# Clone the landportal_model
-cd receiver
+# Clone the landportal-receiver and the model source code
+cd /vagrant
+git clone https://github.com/weso/landportal-receiver.git
+cd landportal-receiver
 git clone https://github.com/weso/landportal_model.git model
 cd ..
-# Install landportal-receiver requirements
-pip install -r /vagrant/virtualenvs/receiver/src/receiver/requirements.txt
+# Install Receiver into a Python virtual environment
+mkdir -p /vagrant/virtualenvs/receiver
+virtualenv /vagrant/virtualenvs/receiver
+. /vagrant/virtualenvs/receiver/bin/activate #Activate the virtualenv
+cd /vagrant/virtualenvs/receiver
+pip install -r /vagrant/landportal-receiver/requirements.txt
 deactivate
-. /vagrant/virtualenvs/receiver/bin/activate
+. /vagrant/virtualenvs/receiver/bin/activate #Reload the virtualenv
+# Create and initialize the database schema
+cd /vagrant/landportal-receiver
+python create_db.py
--- scripts/receiver.wsgi
+++ scripts/receiver.wsgi
@@ -5,5 +5,5 @@ import sys
 activate_this = '/vagrant/virtualenvs/receiver/bin/activate_this.py'
 execfile(activate_this, dict(__file__=activate_this))
 # Import landportal-receiver application
-sys.path.insert(0, '/vagrant/virtualenvs/receiver/src/receiver/')
+sys.path.insert(0, '/vagrant/landportal-receiver/')
 from app import app as application
