b2f642d3f33009775f821a3bf39342a8bd66ffb2
belaustegui <cristian.alvb@gmail.com>
2014-04-10 15:20:10 +0200

    Update API installation script to clone submodule. See #21

--- scripts/api.sh
+++ scripts/api.sh
@@ -8,7 +8,7 @@ echo 'API already configured!. Delete this to reconfigure API stack.' > /vagrant
 
 # Clone the landportal-receiver and the model source code
 cd /vagrant
-git clone https://github.com/weso/landportal-data-access-api.git
+git clone --recursive https://github.com/weso/landportal-data-access-api.git
 # Install API into a Python virtual environment
 mkdir -p /vagrant/virtualenvs/api
 virtualenv /vagrant/virtualenvs/api
